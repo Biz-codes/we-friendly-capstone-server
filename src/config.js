4448d886f67690106ba5345cc227eb7bf0bc947a
Biz Hight <elizabeth.biz.hight@gmail.com>
2021-06-17 18:03:22 -0400

    renamed test database

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://biz@localhost/we_friendly',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://biz@localhost/we_friendly-test',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://biz@localhost/we_friendly_test',
   JWT_SECRET: process.env.JWT_SECRET || 'Hazel',
   // API_TOKEN: process.env.API_TOKEN || '',
   CLIENT_ORIGIN: '*',
